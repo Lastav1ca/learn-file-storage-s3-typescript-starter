9e1f2be956c2dc0efca01e40fba2f8eae5e9d07d
Lastav1ca <stefandmd22@gmail.com>
2026-09-15 17:26:36 +0200

    added saving videos to s3

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,8 +1,61 @@
 import { respondWithJSON } from "./json";
 
 import { type ApiConfig } from "../config";
-import type { BunRequest } from "bun";
+import { type BunRequest } from "bun";
+import { getBearerToken, validateJWT } from "../auth";
+import { getVideo, updateVideo } from "../db/videos";
+import { UserForbiddenError, NotFoundError, BadRequestError } from "./errors";
+import path from "node:path";
+
+const MAX_UPLOAD_SIZE = 1 << 30;
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
-  return respondWithJSON(200, null);
+  const { videoId } = req.params as { videoId?: string };
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userId = validateJWT(token, cfg.jwtSecret)
+
+  const video = getVideo(cfg.db, videoId)
+
+  if (!video) {
+    throw new NotFoundError("Couldn't find video");
+  }
+  if (userId !== video.userID){
+    throw new UserForbiddenError("You are not this videos owner!")
+  }
+
+  const formData = await req.formData()
+  const file = formData.get("video")
+  
+  if (!(file instanceof File)){
+    throw new BadRequestError("File found is not a video")
+  }
+
+  if (file.size > MAX_UPLOAD_SIZE){
+    throw new BadRequestError("Video is too large")
+  }
+
+  if (file.type !== 'video/mp4'){
+    throw new BadRequestError("File must be a video/mp4")
+  }
+
+  const videoKey = `${videoId}.mp4`
+
+  const tempPath = path.join("/tmp", videoKey)
+
+  try{
+    await Bun.write(tempPath, file)
+
+    await cfg.s3Client.file(videoKey).write(Bun.file(tempPath), {type : file.type})
+
+    video.videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${videoKey}`
+
+    updateVideo(cfg.db, video)
+  }finally{
+    await Bun.file(tempPath).delete()
+  }
+  return respondWithJSON(200, video);
 }
